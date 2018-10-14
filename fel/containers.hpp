a889d8aabd7eee6638d01e4323ab0b0bf3ac8791
wegamekinglc <scrappedprince.li@gmail.com>
2018-10-14 23:35:06 +0800

    modifed series and added its tests

--- fel/containers.hpp
+++ fel/containers.hpp
@@ -7,16 +7,108 @@ namespace FEngine {
 
     class Series {
         public:
+            using const_iterator = map<string, double>::const_iterator;
             Series(const vector<string>& keys, const vector<double>& vals) {
                 for (size_t i = 0; i < keys.size(); ++i)
                     vals_[keys[i]] = vals[i];
+            }
+
+            Series(const map<string, double>& vals) {
+                vals_ = vals;
             }
 
             Series(string key, double val) {
                 vals_[key] = val;
             }
 
+            double& operator[](const string& key) { return vals_[key];}
+            double at(const string& key) const { return vals_.at(key);}
+            size_t size() const { return vals_.size();}
+            const_iterator find(const string& key) const { return vals_.find(key);}
+            const_iterator begin() const { return vals_.begin();}
+            const_iterator end() const { return vals_.end();}
+
         private:
             map<string, double> vals_;
     };
+
+    Series operator+(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val += rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = it->second;
+        }
+        
+        return Series(res);
+    }
+
+    Series operator-(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val -= rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = -it->second;
+        }
+        
+        return Series(res);
+    }
+
+    Series operator*(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val *= rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = it->second;
+        }
+    
+        return Series(res);
+    }
+
+    Series operator/(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val /= rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = 1. / it->second;
+        }
+    
+        return Series(res);
+    }
 }
---
+++ tests/containers.cpp
@@ -0,0 +1,153 @@
+#include <fel/containers.hpp>
+#include <gtest/gtest.h>
+
+using namespace FEngine;
+
+TEST(SeriesTest, TestGet) {
+    vector<string> keys = {"a", "b"};
+    vector<double> vals = {2., 1.};
+
+    Series res(keys, vals);
+    ASSERT_EQ(res["a"], 2.);
+    ASSERT_EQ(res["b"], 1.);
+}
+
+TEST(SeriesTest, TestAt) {
+    vector<string> keys = {"a", "b"};
+    vector<double> vals = {2., 1.};
+
+    Series res(keys, vals);
+    ASSERT_EQ(res.at("a"), 2.);
+    ASSERT_EQ(res.at("b"), 1.);   
+}
+
+TEST(SeriesTest, TestSize) {
+    vector<string> keys = {"a", "b"};
+    vector<double> vals = {2., 1.};
+
+    Series res(keys, vals);
+    ASSERT_EQ(res.size(), 2);   
+}
+
+TEST(SeriesTest, TestIteration) {
+    vector<string> keys = {"a", "b", "c"};
+    vector<double> vals = {2., 1., 3.};
+
+    Series res(keys, vals);
+    int counts = 0;
+    for(Series::const_iterator it = res.begin(); it != res.end(); ++it) {
+        ++counts;
+        if(it->first == "a")
+            ASSERT_EQ(it->second, 2.);
+        else if (it->first == "b")
+            ASSERT_EQ(it->second, 1.);
+        else
+            ASSERT_EQ(it->second, 3.);
+        
+    }
+    ASSERT_EQ(counts, 3);
+}
+
+TEST(SeriesTest, TestPlus) {
+    vector<string> keys1 = {"a", "b", "c"};
+    vector<double> vals1 = {2., 1., 3.};
+    Series lhs(keys1, vals1);
+
+    vector<string> keys2 = {"b", "c", "d"};
+    vector<double> vals2 = {2., 1., 3.};
+    Series rhs(keys2, vals2);
+
+    Series res = lhs + rhs;
+    for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+        string key = it->first;
+        double val1 = it->second;
+        if(rhs.find(key) != rhs.end())
+            ASSERT_EQ(res[key], val1 + rhs[key]);
+        else
+            ASSERT_EQ(res[key], val1);
+    }
+
+    for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+        string key = it->first;
+        if(lhs.find(key) == lhs.end())
+            ASSERT_EQ(res[key], it->second);
+    }
+}
+
+TEST(SeriesTest, TestMinus) {
+    vector<string> keys1 = {"a", "b", "c"};
+    vector<double> vals1 = {2., 1., 3.};
+    Series lhs(keys1, vals1);
+
+    vector<string> keys2 = {"b", "c", "d"};
+    vector<double> vals2 = {2., 1., 3.};
+    Series rhs(keys2, vals2);
+
+    Series res = lhs - rhs;
+    for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+        string key = it->first;
+        double val1 = it->second;
+        if(rhs.find(key) != rhs.end())
+            ASSERT_EQ(res[key], val1 - rhs[key]);
+        else
+            ASSERT_EQ(res[key], val1);
+    }
+
+    for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+        string key = it->first;
+        if(lhs.find(key) == lhs.end())
+            ASSERT_EQ(res[key], -it->second);
+    }
+}
+
+TEST(SeriesTest, TestMultiply) {
+    vector<string> keys1 = {"a", "b", "c"};
+    vector<double> vals1 = {2., 1., 3.};
+    Series lhs(keys1, vals1);
+
+    vector<string> keys2 = {"b", "c", "d"};
+    vector<double> vals2 = {2., 1., 3.};
+    Series rhs(keys2, vals2);
+
+    Series res = lhs * rhs;
+    for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+        string key = it->first;
+        double val1 = it->second;
+        if(rhs.find(key) != rhs.end())
+            ASSERT_EQ(res[key], val1 * rhs[key]);
+        else
+            ASSERT_EQ(res[key], val1);
+    }
+
+    for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+        string key = it->first;
+        if(lhs.find(key) == lhs.end())
+            ASSERT_EQ(res[key], it->second);
+    }
+}
+
+TEST(SeriesTest, TestDivide) {
+    vector<string> keys1 = {"a", "b", "c"};
+    vector<double> vals1 = {2., 1., 3.};
+    Series lhs(keys1, vals1);
+
+    vector<string> keys2 = {"b", "c", "d"};
+    vector<double> vals2 = {2., 1., 3.};
+    Series rhs(keys2, vals2);
+
+    Series res = lhs / rhs;
+    for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+        string key = it->first;
+        double val1 = it->second;
+        if(rhs.find(key) != rhs.end())
+            ASSERT_EQ(res[key], val1 / rhs[key]);
+        else
+            ASSERT_EQ(res[key], val1);
+    }
+
+    for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+        string key = it->first;
+        if(lhs.find(key) == lhs.end())
+            ASSERT_EQ(res[key], 1. / it->second);
+    }
+}

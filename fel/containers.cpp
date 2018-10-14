e4ba15f6f99f9f95a2458d0e459fb243595d922a
wegamekinglc <scrappedprince.li@gmail.com>
2018-10-15 00:00:30 +0800

    fixed multiple definition bug

---
+++ fel/containers.cpp
@@ -0,0 +1,83 @@
+#include <fel/containers.hpp>
+
+namespace FEngine {
+    Series operator+(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val += rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = it->second;
+        }
+        
+        return Series(res);
+    }
+
+    Series operator-(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val -= rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = -it->second;
+        }
+        
+        return Series(res);
+    }
+
+    Series operator*(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val *= rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = it->second;
+        }
+    
+        return Series(res);
+    }
+
+    Series operator/(const Series& lhs, const Series& rhs) {
+        map<string, double> res;
+
+        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
+            string key = it->first;
+            double val = it->second;
+            if(rhs.find(key) != rhs.end())
+                val /= rhs.at(key);
+            res[key] = val;
+        }
+
+        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
+            string key = it->first;
+            if(lhs.find(key) == lhs.end())
+                res[key] = 1. / it->second;
+        }
+    
+        return Series(res);
+    }
+}
--- fel/containers.hpp
+++ fel/containers.hpp
@@ -3,8 +3,7 @@
 #include <fel/types.hpp>
 
 namespace FEngine {
-    using DataPack = map<string, map<string, map<DateTime, double>>>;
-
+    
     class Series {
         public:
             using const_iterator = map<string, double>::const_iterator;
@@ -32,83 +31,11 @@ namespace FEngine {
             map<string, double> vals_;
     };
 
-    Series operator+(const Series& lhs, const Series& rhs) {
-        map<string, double> res;
+    Series operator+(const Series& lhs, const Series& rhs);
 
-        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
-            string key = it->first;
-            double val = it->second;
-            if(rhs.find(key) != rhs.end())
-                val += rhs.at(key);
-            res[key] = val;
-        }
+    Series operator-(const Series& lhs, const Series& rhs);
 
-        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
-            string key = it->first;
-            if(lhs.find(key) == lhs.end())
-                res[key] = it->second;
-        }
-        
-        return Series(res);
-    }
+    Series operator*(const Series& lhs, const Series& rhs);
 
-    Series operator-(const Series& lhs, const Series& rhs) {
-        map<string, double> res;
-
-        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
-            string key = it->first;
-            double val = it->second;
-            if(rhs.find(key) != rhs.end())
-                val -= rhs.at(key);
-            res[key] = val;
-        }
-
-        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
-            string key = it->first;
-            if(lhs.find(key) == lhs.end())
-                res[key] = -it->second;
-        }
-        
-        return Series(res);
-    }
-
-    Series operator*(const Series& lhs, const Series& rhs) {
-        map<string, double> res;
-
-        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
-            string key = it->first;
-            double val = it->second;
-            if(rhs.find(key) != rhs.end())
-                val *= rhs.at(key);
-            res[key] = val;
-        }
-
-        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
-            string key = it->first;
-            if(lhs.find(key) == lhs.end())
-                res[key] = it->second;
-        }
-    
-        return Series(res);
-    }
-
-    Series operator/(const Series& lhs, const Series& rhs) {
-        map<string, double> res;
-
-        for(Series::const_iterator it = lhs.begin(); it != lhs.end(); ++it) {
-            string key = it->first;
-            double val = it->second;
-            if(rhs.find(key) != rhs.end())
-                val /= rhs.at(key);
-            res[key] = val;
-        }
-
-        for(Series::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
-            string key = it->first;
-            if(lhs.find(key) == lhs.end())
-                res[key] = 1. / it->second;
-        }
-    
-        return Series(res);
-    }
+    Series operator/(const Series& lhs, const Series& rhs);
 }

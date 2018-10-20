5cf738d3e73f1b85a2165c086b062671df6c0a1f
wegamekinglc <scrappedprince.li@gmail.com>
2018-10-20 11:10:48 +0800

    added more tests for operators

--- tests/nodes.cpp
+++ tests/nodes.cpp
@@ -40,3 +40,63 @@ TEST(TestNodes, TestPlusOperator) {
     ASSERT_EQ(res["aapl"], 4.);
     ASSERT_EQ(res["ibm"], 6.);
 }
+
+TEST(TestNodes, TestMinusOperator) {
+    DataPack data;
+    data["aapl"] = map<string, map<DateTime, double>>();
+    data["ibm"] = map<string, map<DateTime, double>>();
+    data["aapl"]["close"] = map<DateTime, double>();
+    data["ibm"]["close"] = map<DateTime, double>();
+    data["aapl"]["close"]["2018-01-05"] = 2.;
+    data["ibm"]["close"]["2018-01-05"] = 3.;
+
+    GlobalSymbols symbols({"aapl", "ibm"});
+    Last close1("close");
+    Last close2("close");
+
+    MinusOperator combined = close1 - close2;
+    Series res = combined.calculate(data, "2018-01-05");
+
+    ASSERT_EQ(res["aapl"], 0.);
+    ASSERT_EQ(res["ibm"], 0.);
+}
+
+TEST(TestNodes, TestMultiplyOperator) {
+    DataPack data;
+    data["aapl"] = map<string, map<DateTime, double>>();
+    data["ibm"] = map<string, map<DateTime, double>>();
+    data["aapl"]["close"] = map<DateTime, double>();
+    data["ibm"]["close"] = map<DateTime, double>();
+    data["aapl"]["close"]["2018-01-05"] = 2.;
+    data["ibm"]["close"]["2018-01-05"] = 3.;
+
+    GlobalSymbols symbols({"aapl", "ibm"});
+    Last close1("close");
+    Last close2("close");
+
+    MultiplyOperator combined = close1 * close2;
+    Series res = combined.calculate(data, "2018-01-05");
+
+    ASSERT_EQ(res["aapl"], 4.);
+    ASSERT_EQ(res["ibm"], 9.);
+}
+
+TEST(TestNodes, TestDivideOperator) {
+    DataPack data;
+    data["aapl"] = map<string, map<DateTime, double>>();
+    data["ibm"] = map<string, map<DateTime, double>>();
+    data["aapl"]["close"] = map<DateTime, double>();
+    data["ibm"]["close"] = map<DateTime, double>();
+    data["aapl"]["close"]["2018-01-05"] = 2.;
+    data["ibm"]["close"]["2018-01-05"] = 3.;
+
+    GlobalSymbols symbols({"aapl", "ibm"});
+    Last close1("close");
+    Last close2("close");
+
+    DivideOperator combined = close1 / close2;
+    Series res = combined.calculate(data, "2018-01-05");
+
+    ASSERT_EQ(res["aapl"], 1.);
+    ASSERT_EQ(res["ibm"], 1.);
+}

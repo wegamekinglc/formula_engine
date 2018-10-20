dfd0c454426c35ddd1870a176208286fb67221b0
wegamekinglc <scrappedprince.li@gmail.com>
2018-10-20 15:59:03 +0800

    added special names for ohlc

--- fel/fel.hpp
+++ fel/fel.hpp
@@ -3,3 +3,10 @@
 #include <fel/settings.hpp>
 #include <fel/nodes.hpp>
 #include <fel/crosssections.hpp>
+
+namespace FEngine {
+    const auto CLOSE = Last("close");
+    const auto OPEN = Last("open");
+    const auto HIGH = Last("high");
+    const auto LOW = Last("low");
+}

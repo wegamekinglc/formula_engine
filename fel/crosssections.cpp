2800e58eb7b23312b5f9c809984d2924d245d09f
wegamekinglc <scrappedprince.li@gmail.com>
2018-10-20 12:05:10 +0800

    complete implementation  of CSRank

--- fel/crosssections.cpp
+++ fel/crosssections.cpp
@@ -5,11 +5,19 @@ namespace FEngine {
     CSBase::CSBase(const Node& inner)
         :inner_(inner.clone()) {}
 
+    CSRank::CSRank(const Node& inner)
+        :CSBase(inner) {}
+
     Depends CSBase::depends(const DateTime& base) const {
         return inner_->depends(base);
     }
 
     CSRank* CSRank::clone() const {
         return new CSRank(*inner_);
+    }
+
+    Series CSRank::calculate(const DataPack& data, const DateTime& base) const {
+        Series vals = inner_->calculate(data, base);
+        return rank(vals);
     }
 }

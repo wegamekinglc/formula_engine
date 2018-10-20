b49483c1bd8209eee88399523f9d4425dd893bed
wegamekinglc <scrappedprince.li@gmail.com>
2018-10-20 10:41:55 +0800

    modifed the implementation of mergeDepends

--- fel/types.cpp
+++ fel/types.cpp
@@ -1,14 +1,20 @@
+#include<algorithm>
 #include<fel/types.hpp>
 
 namespace FEngine {
     Depends mergeDepends(const Depends& lhs, const Depends& rhs) {
-        // The implementation is simple and not correct
         Depends res = lhs;
         for(Depends::const_iterator it = rhs.begin(); it != rhs.end(); ++it) {
             string key = it->first;
             vector<DateTime> val = it->second;
             if(lhs.find(key) == lhs.end())
                 res[key] = val;
+            else {
+                vector<DateTime>& t = res[key];
+                t.insert(t.end(), val.begin(), val.end());
+                std::sort(t.begin(), t.end() );
+                t.erase(std::unique(t.begin(), t.end() ), t.end() );
+            }
         }
         return res;
     }
--- tests/types.cpp
+++ tests/types.cpp
@@ -8,10 +8,12 @@ TEST(TypeTest, TestDependsMerge) {
     Depends d2 = Depends();
     d1["a"] = {"2018-01-01"};
     d1["b"] = {"2018-03-01"};
+    d2["b"] = {"2018-02-01"};
     d2["c"] = {"2018-05-01"};
 
     Depends res = mergeDepends(d1, d2);
     ASSERT_EQ(res["a"], d1["a"]);
-    ASSERT_EQ(res["b"], d1["b"]);
+    ASSERT_EQ(res["b"][0], "2018-02-01");
+    ASSERT_EQ(res["b"][1], "2018-03-01");
     ASSERT_EQ(res["c"], d2["c"]);
 }

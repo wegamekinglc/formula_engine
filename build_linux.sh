131ac4233748d200c89b32a5af67931fe36745b9
wegamekinglc <scrappedprince.li@gmail.com>
2018-11-03 23:01:13 +0800

    update

--- build_linux.sh
+++ build_linux.sh
@@ -4,6 +4,8 @@ export num_cores=`grep -c processor /proc/cpuinfo`
 export FEL_DIR=$PWD
 export LD_LIBRARY_PATH=$PWD/lib:$PWD/lib/libpqxx/linux:$LD_LIBRARY_PATH
 
+ldd $PWD/lib/libpqxx/linux/libpq.so.5.10
+
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$FEL_DIR ..

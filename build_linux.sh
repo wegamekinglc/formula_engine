317b60bc73c60f13d3b8112c8798bed860a8b7d5
wegamekinglc <scrappedprince.li@gmail.com>
2018-11-04 18:54:37 +0800

    remove linux lib

--- build_linux.sh
+++ build_linux.sh
@@ -4,6 +4,12 @@ export num_cores=`grep -c processor /proc/cpuinfo`
 export FEL_DIR=$PWD
 export LD_LIBRARY_PATH=$PWD/lib:$PWD/lib/libpqxx/linux:$LD_LIBRARY_PATH
 
+cd libpqxx
+./configure --disable-documentation --prefix=$FEL_DIR/lib/libpqxx/linux
+make
+make install
+cd ..
+
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$FEL_DIR ..
